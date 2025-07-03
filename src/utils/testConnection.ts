eacd83d969e7463d69058a2d476b5ffc376d35d1
Alysson Rafael <alyssonrafael11@gmail.com>
2025-07-02 22:40:52 -0300

    update/ melhoria na função de teste de conecxão com o banco deixando tratamento mais amigavel

--- src/utils/testConnection.ts
+++ src/utils/testConnection.ts
@@ -1,12 +1,25 @@
 import prisma from "../prisma";
+import { Prisma } from "@prisma/client";
 
 export async function testConnection() {
   try {
+    console.log("Tentando conectar ao banco de dados...");
     await prisma.$connect();
     console.log("Conexão estabelecida com sucesso!");
-    await prisma.$disconnect();
-    console.log("Desconectado do banco. Teste concluído!");
   } catch (err) {
-    console.error("Erro ao conectar ou realizar operação no banco:", err);
+    console.error("Falha ao conectar ao banco de dados.");
+
+    if (err instanceof Prisma.PrismaClientInitializationError) {
+      console.error("O banco de dados está offline ou inacessível.");
+    } else if (err instanceof Prisma.PrismaClientRustPanicError) {
+      console.error("O Prisma enfrentou um erro interno inesperado.");
+    } else if (err instanceof Prisma.PrismaClientKnownRequestError) {
+      console.error(`Erro conhecido do Prisma: ${err.message}`);
+    } else {
+      console.error("Erro desconhecido:", err);
+    }
+  } finally {
+    await prisma.$disconnect();
+    console.log("Desconectado do banco. Teste concluído.");
   }
 }
